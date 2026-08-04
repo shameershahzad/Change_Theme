2368ea6c6dcae606339a54bdd935ed119d5a38de
Shameer Shahzad <shameershazad156@gmail.com>
2026-08-04 06:01:00 +0500

    Delete src/test/vulnerable-example.js

--- src/test/vulnerable-example.js
+++
@@ -1,8 +0,0 @@
-// Intentionally vulnerable code for testing Sentra's real-data pipeline
-const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
-
-function runUserCommand(userInput) {
-  return eval(userInput);
-}
-
-module.exports = { runUserCommand };
